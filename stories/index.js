696a6086b802256930efaef914e980f3c99836fc
Areej Ba Salamah <areej.ombas@gmail.com>
2020-12-08 15:56:09 -0500

    story for Empty component added

--- stories/index.js
+++ stories/index.js
@@ -12,7 +12,8 @@ import InterviewerListItem from "components/InterviewerListItem.js";
 import InterviewerList from "components/InterviewerList.js";
 import Appointment from "components/Appointment";
 
-import Header from "components/Appointment/Header.js"
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js"
 
 
 
@@ -142,3 +143,4 @@ storiesOf("Button", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", ()=> <Appointment time={ '12pm' }/> )
     .add("Header", () => <Header time={ '12pm' }/>)
+    .add("Empty", () => <Empty onAdd = {action("onAdd")}/>)
