fe2ff488829e4d7b255d1a75c766ec5863c92e8d
Areej Ba Salamah <areej.ombas@gmail.com>
2020-12-09 13:28:03 -0500

    Header,Show and Empty components added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+
 import './styles.scss';
 
 
 export default function Appointment(props) {
 
 return (
-  <article className="appointment"></article>
+  <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? <Show onEdit={props.onEdit}
+    onDelete={props.onDelete}
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}/> : <Empty onAdd={props.onAdd}/>}
+
+
+
+
+
+  </article>
   
   )
 }
