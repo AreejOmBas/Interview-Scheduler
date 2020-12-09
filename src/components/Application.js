88ba8c60536de62fd0a97a161d041302b4c7c882
Areej Ba Salamah <areej.ombas@gmail.com>
2020-12-09 17:18:29 -0500

    api request to days added

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 
@@ -46,33 +47,32 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
 
   const appointmentList = appointments.map( (appointment) => {
     return (
       <Appointment key={appointment.id} {...appointment} />)
   })
+
+  useEffect( () => {
+
+    axios.get('/api/days')
+    .then((response)  => {
+      // handle success
+      setDays([...response.data])
+    })
+    .catch((error) =>  {
+      // handle error
+      console.log(error);
+    })
+
+
+  },[])
 
 
   return (
