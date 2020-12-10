6d355950bec06dddb2776f59b5c030f51df3c6d7
Areej Ba Salamah <areej.ombas@gmail.com>
2020-12-10 12:27:47 -0500

    state object introduced to better manage useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -51,8 +51,14 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+ 
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   const appointmentList = appointments.map( (appointment) => {
     return (
@@ -64,7 +70,7 @@ export default function Application(props) {
     axios.get('/api/days')
     .then((response)  => {
       // handle success
-      setDays([...response.data])
+      setDays(response.data)
     })
     .catch((error) =>  {
       // handle error
@@ -87,8 +93,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
