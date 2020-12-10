386aaa5aad591747d1c85049546c55f3b5ed0a6b
Areej Ba Salamah <areej.ombas@gmail.com>
2020-12-10 18:31:45 -0500

    transition between modes implemented

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,27 +1,44 @@
 import React from 'react'
 
+import useVisualMode from "hooks/useVisualMode";
+
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
+import Form from "components/Appointment/Form.js";
 
 import './styles.scss';
 
 
 export default function Appointment(props) {
 
-return (
-  <article className="appointment">
-    <Header time={props.time}/>
-    {props.interview ? <Show onEdit={props.onEdit}
-    onDelete={props.onDelete}
-    student={props.interview.student}
-    interviewer={props.interview.interviewer}/> : <Empty onAdd={props.onAdd}/>}
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (<Form interviewers={[]}
+        onSave={props.onSave}
+        onCancel={back}/>)}
 
 
 
 
 
-  </article>
-  
+    </article>
+
   )
 }
